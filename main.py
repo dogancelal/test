c70a88e010ecbe0422f3ab8799952c53379bf119
dogancelal <celaldogan@gmal.com>
2021-07-25 12:55:56 +0300

    test

---
+++ main.py
@@ -0,0 +1,13 @@
+
+print("please enter first number")
+first_number=input()
+
+print("your first number is", first_number)
+print("please enter your second number")
+second_number=input()
+
+print("your second number is",second_number)
+
+average= (int(first_number)+int(second_number))/2
+
+print("your average is",average)
